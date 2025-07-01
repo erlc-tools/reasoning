75df2660045b1219abe5d40e657c658effc702ea
Fin M <fin@mace.law>
2025-07-01 19:01:02 -0400

    add logging (and debuging)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import * as erlc from 'erlc';
 import * as dotenv from "dotenv";
+import { Logger } from "tslog";
 
 // post imports
+export const log = new Logger();
 dotenv.config()
+
+const debug_pre = process.env.debug as string;
+export var debug = false as boolean; // fuck compiler errors
+if (debug_pre == "true") { debug = true } else { debug = false };
+
+
+log.info(`DEBUG IS ${debug}`)
