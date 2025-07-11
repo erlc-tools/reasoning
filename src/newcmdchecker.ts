197e56fd6899f2dfa65d0f606cc87bde204e1217
Fin M <fin@mace.law>
2025-07-11 12:05:04 -0400

    add the actual checking

--- src/newcmdchecker.ts
+++ src/newcmdchecker.ts
@@ -2,12 +2,14 @@ import { log, token, client } from ".";
 import { CommandLog } from "erlc";
 import { unix } from "./utils"
 
-let latestcheck = 
+let latestcheck = unix() as number;
 
 async function newcmdchecker(logs: CommandLog[]): Promise<CommandLog[]> {
     let results = [] as CommandLog[]
     logs.forEach(log => {
-
+        if (log.Timestamp > latestcheck) {
+            results.push(log)
+        }
     })
     return results;
 }
