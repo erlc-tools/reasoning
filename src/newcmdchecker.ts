1538befea45ada1f87c85d244f47b0561b9a98e2
Fin M <fin@mace.law>
2025-07-11 12:02:26 -0400

    start making newcmdchecker.ts

--- src/newcmdchecker.ts
+++ src/newcmdchecker.ts
@@ -1,1 +1,12 @@
 import { log, token, client } from ".";
+import { CommandLog } from "erlc";
+
+let latestcheck = 
+
+async function newcmdchecker(logs: CommandLog[]): Promise<CommandLog[]> {
+    let results = [] as CommandLog[]
+    logs.forEach(log => {
+
+    })
+    return results;
+}
