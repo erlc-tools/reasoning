a0df022299af63e096f6ae1ad08e7f784375a05b
Fin M <fin@mace.law>
2025-07-12 16:03:34 -0400

    add caching to debug.ts

--- src/debug.ts
+++ src/debug.ts
@@ -1,6 +1,12 @@
+var _cache: null | boolean = null
+
 export function isDebug(): boolean {
+    if (_cache != null) {
+        return _cache
+    }
     const debug_pre = process.env.debug as string;
     var debug = false as boolean;
     if (debug_pre == "true") { debug = true } else { debug = false };
+    _cache = debug
     return debug
 }
