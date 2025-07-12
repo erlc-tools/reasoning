8635f5b155fb81d15e6644e33b9d642c205df767
Fin M <fin@mace.law>
2025-07-12 15:52:45 -0400

    add debug logging to newcmdchecker

--- src/newcmdchecker.ts
+++ src/newcmdchecker.ts
@@ -1,6 +1,9 @@
 import { CommandLog } from "erlc";
-import { unix } from "./utils"
+import { unix } from "./utils";
+import { isDebug } from "./debug";
+import { Logger } from "tslog";
 
+const debug = isDebug();
 let latestcheck = unix() as number;
 
 export async function newcmdchecker(logs: CommandLog[]): Promise<CommandLog[]> {
@@ -8,6 +11,9 @@ export async function newcmdchecker(logs: CommandLog[]): Promise<CommandLog[]> {
     logs.forEach(log => {
         if (log.Timestamp > latestcheck) {
             results.push(log)
+            if(debug==true){ new Logger().debug("logging this because log.Timestamp > latestcheck. this: ", log)} // please replace this someday
+        } else if (log.Timestamp <= latestcheck) {
+            if(debug==true){ new Logger().debug(" Not logging this because log.Timestamp <= latestcheck. this: ", log)} // please replace this someday
         }
     })
     latestcheck = unix() as number
