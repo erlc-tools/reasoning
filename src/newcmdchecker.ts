50d2bdbbdb31195578ca4f2b61ce627a18d737a6
Fin M <fin@mace.law>
2025-07-17 13:23:22 -0400

    create too much debugging for when u need a lot of debugging (in newcmdcheck only)

--- src/newcmdchecker.ts
+++ src/newcmdchecker.ts
@@ -7,15 +7,17 @@ import { hi } from "./funchellos"
 const debug = isDebug();
 let latestcheck = unix() as number;
 
+const tooMuchDebugging = true as boolean;
+
 export async function newcmdchecker(logs: CommandLog[]): Promise<CommandLog[]> {
     hi("newcmdchecker")
     let results = [] as CommandLog[]
     logs.forEach(log => {
         if (log.Timestamp > latestcheck) {
             results.push(log)
-            //if(debug==true){ logger.debug("logging this because log.Timestamp > latestcheck. this: ", log)}
+            if(tooMuchDebugging==true){ logger.debug("logging this because log.Timestamp > latestcheck. this: ", log)}
         } else if (log.Timestamp <= latestcheck) {
-            //if(debug==true){ logger.debug(" Not logging this because log.Timestamp <= latestcheck. this: ", log)}
+            if(tooMuchDebugging==true){ logger.debug(" Not logging this because log.Timestamp <= latestcheck. this: ", log)}
         }
     })
     latestcheck = unix() as number
