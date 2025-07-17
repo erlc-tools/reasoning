cc1127b949f6c3f6429d12b6d76dc86ade8bbd8e
Fin M <fin@mace.law>
2025-07-17 13:55:38 -0400

    sort all logs into either actionCmds or reasonCmds

--- src/commandbinder.ts
+++ src/commandbinder.ts
@@ -5,9 +5,26 @@ type CommandBinding = {
     reason: CommandLog
 }
 
+export function isReasonCmd(log: CommandLog): boolean {
+    if (log.Command.includes(":log reasoning ")) {
+        return true;
+    }
+    return false;
+}
+
+
 export function commandBinder(logs: CommandLog[]): CommandBinding[] {
+    // go through each log, and check if its an action cmd or a reason cmd
+    let actionCmds = [] as CommandLog[];
+    let reasonCmds = [] as CommandLog[];
 
-
+    logs.forEach(log => {
+        if (isReasonCmd(log)==true) {
+            reasonCmds.push(log)
+        } else {
+            actionCmds.push(log)
+        }
+    })
 
     
 }
